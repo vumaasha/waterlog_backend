6819e5418f1789f7fa6ea78fe80bb57a9cb2f542
Venki <venkatesh.umaashankar@xoanonanalytics.com>
2015-12-18 15:46:59 +0530

    removed requires from setup.py

--- setup.py
+++ setup.py
@@ -8,16 +8,7 @@ with open(os.path.join(here, 'README.txt')) as f:
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
-requires = [
-    'pyramid',
-    'pyramid_chameleon',
-    'pyramid_debugtoolbar',
-    'pyramid_tm',
-    'SQLAlchemy',
-    'transaction',
-    'zope.sqlalchemy',
-    'waitress',
-    ]
+requires = []
 
 setup(name='waterlog_py',
       version='0.0',
